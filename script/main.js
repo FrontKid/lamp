aa39d98eb5af22ed6e018ea560c04e36f4b48a52
Denys Poltorak <itwfrontend@gmail.com>
2022-09-24 13:35:46 +0300

    header all copmpleted

--- script/main.js
+++ script/main.js
@@ -1,14 +1,17 @@
 const closeNav = document.getElementById('header__nav-burger-hub-1-field')
 
 closeNav.addEventListener("click", () => {
-   const navPopUp = document.getElementById('header__nav-burger')
-   const navPopUpValues = window.getComputedStyle(navPopUp)
-   console.log(navPopUpValues.left);
+  // const navPopUp = document.getElementById('header__nav-burger')
+  // const navPopUpValues = window.getComputedStyle(navPopUp)
+  // console.log(navPopUpValues.left);
 
-   if (navPopUpValues.left !== '0px') {
-      navPopUp.style.left = "0"
-   } else {
-      navPopUp.style.left = "-100%"
-   }
-
+  // if (navPopUpValues.left !== '0px') {
+  //    navPopUp.style.left = "0"
+  // } else {
+  //    navPopUp.style.left = "-100%"
+  // }
+  const navPopUp = document.querySelector('.header__nav-burger')
+  const body = document.getElementById('body')
+  navPopUp.classList.toggle('header__nav-burger--active')
+  body.classList.toggle('header__nav-burger--hideBody')
 })
