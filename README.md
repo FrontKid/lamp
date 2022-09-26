b67ee29e5ce77636f45f7087cf72ace011e84cb3
Denys Poltorak <itwfrontend@gmail.com>
2022-09-26 13:27:08 +0300

    responsive slider completed look at readme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ online store
 Created by:
   Denys:
     -header
+    -slider
 
 
 
@@ -12,3 +13,4 @@ To Change:
   Denys:
     Disable hover effect on touch screen JQ
     change navbar-burger's icon when it's opened
+    Set up preloader on slider photo's (fix bugs)
--- script/main.js
+++ script/main.js
@@ -25,7 +25,32 @@ $(document).ready(() => {
     slidesToShow: 1,
     adaptiveHeight: true,
     cssEase: 'linear',
-    mobileFirst: true
+    prevArrow: `
+      <button type="button" class="slick-prev">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 12H5" stroke="#454950" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M12 5L5 12L12 19" stroke="#454950" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>
+    `,
+    nextArrow: `
+      <button type="button" class="slick-next">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5 12H19" stroke="#454950" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M12 5L19 12L12 19" stroke="#454950" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>      
+      </button>
+    `,
+    responsive: [
+
+      {
+        breakpoint: 570,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   })
 })
 
