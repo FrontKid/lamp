e8fc8892b192395ce891cea76f9307884dcf7e13
Denys Poltorak <itwfrontend@gmail.com>
2022-10-06 19:36:05 +0300

    reg and log Pop-up was added

--- script/main.js
+++ script/main.js
@@ -102,28 +102,59 @@ function initMap() {
 //*********MAP END***********/
 //*****************************/
 
-function registrationField() {
-  const profiles = [document.querySelector('.header__main-icon'),
-  document.getElementById('profile-mob')]
-
-  const reg = document.querySelector('.reg')
-  const body = document.getElementById('body')
-
-  profiles.forEach(prof => prof.addEventListener('click', (e) => {
-
+//*****************************/
+//**RENDER POP-UP FORM START***/
+//*****************************/
+const body = document.getElementById('body')
+const log = document.querySelector('.log')
+function openClosePopUp(clickOn) {
+  clickOn.addEventListener('click', (e) => {
     e.preventDefault()
-    reg.classList.add('open-pop-up')
-    body.classList.add('hide-body')
-  }))
-
-  reg.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (e.target.classList.contains('reg')) {
-      reg.classList.remove('open-pop-up')
+    if (e.target.classList.contains(clickOn.classList[0])) {
+      clickOn.classList.remove('open-pop-up')
       body.classList.remove('hide-body')
     } else {
       return false
     }
   })
 }
+function logInField() {
+  const entry = document.querySelector('.header__main-icon')
+  entry.addEventListener('click', (e) => {
+    e.preventDefault()
+    log.classList.add('open-pop-up')
+    body.classList.add('hide-body')
+  })
+  // render registration Pop-up
+  openClosePopUp(log)
+}
+logInField()
+function openClosePopUp(clickOn) {
+  clickOn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (e.target.classList.contains(clickOn.classList[0])) {
+      clickOn.classList.remove('open-pop-up')
+      body.classList.remove('hide-body')
+    } else {
+      return false
+    }
+  })
+}
+function registrationField() {
+  const createAcc = document.getElementById('log__form-btn-create')
+  const reg = document.querySelector('.reg')
+  createAcc.addEventListener('click', (e) => {
+    e.preventDefault()
+    setTimeout(() => {
+      reg.classList.add('open-pop-up')
+    }, 200)
+    body.classList.add('hide-body')
+    log.classList.remove('open-pop-up')
+  })
+  // render registration Pop-up
+  openClosePopUp(reg)
+}
 registrationField()
+//*****************************/
+//****RENDER POP-UP FORM END***/
+//*****************************/
